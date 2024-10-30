840f18fab00a007df09d306e9b66449e65b8f09c
dave frost <dfrost3374@gmail.com>
2024-10-30 19:08:57 -0400

    hw 05

--- hw-02a/TestTriangle.py
+++ hw-02a/TestTriangle.py
@@ -9,7 +9,7 @@ The primary goal of this file is to demonstrate a simple unittest implementation
 
 import unittest
 
-from Triangle import classifyTriangle
+from Triangle import classify_triangle
 
 # This code implements the unit test functionality
 # https://docs.python.org/3/library/unittest.html has a nice description of the framework
@@ -17,39 +17,31 @@ from Triangle import classifyTriangle
 class TestTriangles(unittest.TestCase):
     # Right triangles
     def testRightTriangleA(self): 
-        self.assertEqual(classifyTriangle(3, 4, 5), 'Right', '3,4,5 is a Right triangle')
+        self.assertEqual(classify_triangle(3, 4, 5), 'Right', '3,4,5 is a Right triangle')
 
     def testRightTriangleB(self): 
-        self.assertEqual(classifyTriangle(5, 3, 4), 'Right', '5,3,4 is a Right triangle')
+        self.assertEqual(classify_triangle(5, 3, 4), 'Right', '5,3,4 is a Right triangle')
 
     # Equilateral triangles
     def testEquilateralTriangles(self): 
-        self.assertEqual(classifyTriangle(1, 1, 1), 'Equilateral', '1,1,1 should be equilateral')
+        self.assertEqual(classify_triangle(1, 1, 1), 'Equilateral', '1,1,1 should be equilateral')
 
     # Isosceles triangles
     def testIsoscelesTriangleA(self):
-        self.assertEqual(classifyTriangle(2, 2, 3), 'Isosceles', '2,2,3 should be isosceles')
+        self.assertEqual(classify_triangle(2, 2, 3), 'Isosceles', '2,2,3 should be isosceles')
 
     def testIsoscelesTriangleB(self):
-        self.assertEqual(classifyTriangle(3, 2, 2), 'Isosceles', '3,2,2 should be isosceles')
+        self.assertEqual(classify_triangle(3, 2, 2), 'Isosceles', '3,2,2 should be isosceles')
 
     # Scalene triangles
     def testScaleneTriangle(self):
-        self.assertEqual(classifyTriangle(4, 6, 9), 'Scalene', '4,6,9 should be scalene')
+        self.assertEqual(classify_triangle(4, 6, 9), 'Scalene', '4,6,9 should be scalene')
 
     # Invalid triangles
     def testInvalidTriangleA(self):
-        self.assertEqual(classifyTriangle(1, 2, 3), 'NotATriangle', '1,2,3 is not a valid triangle')
+        self.assertEqual(classify_triangle(1, 2, 3), 'NotATriangle', '1,2,3 is not a valid triangle')
 
-    def testInvalidTriangleB(self):
-        self.assertEqual(classifyTriangle(0, 1, 1), 'NotATriangle', '0,1,1 is not a valid triangle')
 
-    def testInvalidTriangleC(self):
-        self.assertEqual(classifyTriangle(-1, 1, 2), 'NotATriangle', '-1,1,2 is not a valid triangle')
-
-    # Floating point precision
-    def testFloatingPointTriangle(self):
-        self.assertEqual(classifyTriangle(0.3, 0.4, 0.5), 'Right', '0.3,0.4,0.5 should be a Right triangle')
 
 if __name__ == '__main__':
     unittest.main()
